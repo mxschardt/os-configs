8710b9e386a64dc5b64d0c89377c61f08a721106
Maxim Schardt <74490624+mxschardt@users.noreply.github.com>
2022-06-06 13:15:18 +0300

    Create os-setup.sh

---
+++ os-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Update && Upgrade 
+sudo apt update && sudo apt upgrade 
+
+# Install essentials 
+sudo apt install man vim htop neofetch git tmux curl
+
+### Configs ###
+# tmux
+curl https://raw.githubusercontent.com/mxschardt/os-configs/main/.tmux.conf > ~/.tmux.conf
+# vim 
+curl https://raw.githubusercontent.com/mxschardt/os-configs/main/.vimrc > ~/.vimrc
+
+### Terminal setup ### 
+
+# Font and zsh
+sudo apt install fonts-firacode zsh 
+
+# Set zsh as default
+chsh -s $(which zsh)
+
+# Oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+# curl https://raw.githubusercontent.com/mxschardt/os-configs/main/.zshrc > ~/.zshrc
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+# Spaceship theme
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sed -i "s/ZSH_THEME=\".*\"/ZSH_THEME=\"spaceship\"/g" ~/.zshrc
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i "s/plugins=(.*)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g" ~/.zshrc
